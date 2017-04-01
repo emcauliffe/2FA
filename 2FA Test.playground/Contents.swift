f949612be8ce33497f3695e91f5b86b7b5306c62
Ethan McAuliffe <ethan@mcauliffe-657.local>
2017-04-01 18:25:36 -0400

    Attempting adding the BigInteger tool/library, re-configuring the launch screen using AutoLayout

--- 2FA Test.playground/Contents.swift
+++ 2FA Test.playground/Contents.swift
@@ -3,8 +3,47 @@
 import UIKit
 import Foundation
 
-var str = "Hello, playground"
-let data = "hello"
-data.sha1()
-"comrade".sha1()
-data.md5()
+var test: = 5
+
+extension String {
+	public func base32Decode() -> IntMax{
+		let value : [Character] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","2","3","4","5","6","7"]
+		var outputInteger:IntMax = 0
+		var multiplier = 1
+		for character in self.characters.reversed() {
+//			print(character)
+//			print(outputInteger)
+			print(Int(value.index(of: character)!) * multiplier)
+			outputInteger += Int(value.index(of: character)!) * multiplier
+			multiplier = multiplier*32
+		}
+		return outputInteger
+	}
+}
+
+
+var dataString = "srqxbd2jahfx5xhpka4lzdnrntg6ynkh"
+
+let time = Int(Date().timeIntervalSince1970)/30
+
+dataString = dataString.uppercased()
+
+dataString.characters.count
+
+dataString
+
+time
+
+//let test1 = String(0xFFFFFFFFFFFFFFF)
+//let test2 = String(0xFFFFFFFFFFFFFFF+100000000)
+//test1
+//test2
+
+
+//dataString.base32Decode()
+
+var test = String(9223372036854775806 + 9223372036854775806)
+
+//let bytes = Array("string".utf8)
+
+//data.append(time)//add u/data = data.sha1()//hmac part 1
---
+++ testingBigInteger.swift
@@ -0,0 +1,12 @@
+//
+//  testingBigInteger.swift
+//  2 Factor Authentication
+//
+//  Created by Ethan on 2017-04-01.
+//  Copyright © 2017 Ethan. All rights reserved.
+//
+
+import Foundation
+
+var test = 12
+let testBig = BInt(test)
